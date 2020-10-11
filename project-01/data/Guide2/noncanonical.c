8a2751ea51f6c6b78d9863bc3b914cc09588b514
Eduardo Brito <up201806271@fe.up.pt>
2020-10-11 18:15:18 +0100

    Added Correct Serial Port Numbers

--- project-01/data/Guide2/noncanonical.c
+++ project-01/data/Guide2/noncanonical.c
@@ -58,7 +58,7 @@ int main(int argc, char **argv)
 
   /* 
     VTIME e VMIN devem ser alterados de forma a proteger com um temporizador a 
-    leitura do(s) pr�ximo(s) caracter(es)
+    leitura do(s) proximo(s) caracter(es)
   */
 
   tcflush(fd, TCIOFLUSH);
